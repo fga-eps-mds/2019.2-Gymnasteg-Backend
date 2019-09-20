a010238f880dce5db10a38032a7c8a4c201f9fb5
Fellipe Araujo <araujo.fellipe@hotmail.com>
2019-09-20 01:18:27 -0300

    Added automatic password generation for judges.
    Co-authored-by: Matheus Felizola <matheusbsb2009@hotmail.com>

--- src/app/controllers/JudgeManagement.js
+++ src/app/controllers/JudgeManagement.js
@@ -1,13 +1,15 @@
 import Judge from "../models/Judge";
 import Database from "../../database";
+import PasswordGenerator from "password-generator";
 
 module.exports = {
   async create(req, res) {
     const { name, email } = req.body;
     try {
       await Database.connection.sync;
-      await Judge.create({ name, email, password: "121212" });
-      return res.status(201).json({ password: "1234" });
+      const generatedPassword = PasswordGenerator(12, true);
+      await Judge.create({ name, email, password: generatedPassword });
+      return res.status(201).json({ password: generatedPassword });
     } catch (error) {
       return res.status(401).send();
     }

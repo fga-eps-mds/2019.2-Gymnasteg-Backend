038dfa83ba8dfa8daae94124d940bac3ca203301
Fellipe Araujo <araujo.fellipe@hotmail.com>
2019-09-26 22:38:51 -0300

    Created athlete migration
    Co-authored-by: Alan Marques Sousa <alan.sousa@protonmail.com>

---
+++ src/database/migrations/20190927012802-create-athletes.js
@@ -0,0 +1,47 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('athletes', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+
+      gender: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+
+      date_born: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('athletes');
+  },
+};

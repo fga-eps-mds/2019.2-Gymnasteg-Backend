9a0216843318160d5875740a51a9c1b723697042
Alan Marques Sousa <alan.sousa@protonmail.com>
2019-10-26 15:04:04 -0300

    Creation vote model
    Co-authored-by: Matheus Felizola <matheusbsb2009@hotmail.com>

---
+++ src/app/models/Vote.js
@@ -0,0 +1,24 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Vote extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        punctuation: Sequelize.DOUBLE,
+        type_punctuation: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Stand, { foreignKey: 'fk_stand_id' });
+    this.belongsTo(models.Judge, { foreignKey: 'fk_judge_id' });
+    this.belongsTo(models.Athlete, { foreignKey: 'fk_athlete_id' });
+  }
+}
+
+export default Vote;

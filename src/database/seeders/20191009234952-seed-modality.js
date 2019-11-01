ea14c6805beca3230526fecd17b5ae9fe253e289
Matheus Felizola Freires <matheusbsb2009@hotmail.com>
2019-11-01 00:52:18 -0300

    Fixed modality seed

--- src/database/seeders/20191009234952-seed-modality.js
+++ src/database/seeders/20191009234952-seed-modality.js
@@ -22,6 +22,7 @@ module.exports = {
         },
 
         {
+          id: 3,
           type: 'Salto sobre cavalo',
           url_image:
             'https://infograficos.estadao.com.br/uploads/galerias/3863/39539.JPG',
@@ -30,7 +31,7 @@ module.exports = {
         },
 
         {
-          id: 3,
+          id: 4,
           type: 'Barras paralelas',
           url_image:
             'https://ogimg.infoglobo.com.br/in/19933124-c58-275/FT1500A/690/x201608161434479708_RTS.jpg.pagespeed.ic.G9AhW3HJat.jpg',
@@ -39,7 +40,7 @@ module.exports = {
         },
 
         {
-          id: 4,
+          id: 5,
           type: 'Barra fixa',
           url_image:
             'http://2.bp.blogspot.com/-ra84jhr85yw/Tk8VI_wlWsI/AAAAAAAABNk/JcBTG4M3x9A/s400/dane.jpg',
@@ -48,7 +49,7 @@ module.exports = {
         },
 
         {
-          id: 5,
+          id: 6,
           type: 'Trave de equilíbrio',
           url_image:
             'https://sportsregras.com/wp-content/uploads/2016/06/ginastica-trave-equilibrio-e1465838239516.webp',
